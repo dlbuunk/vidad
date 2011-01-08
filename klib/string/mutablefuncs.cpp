d0f0927a915257e2614917802794598c9b7f5344
Anton Golov <kdesevis@gmail.com>
2011-01-08 23:38:19 +0100

    Commented code and added licenses where needed.
    Also created a README for klib.

--- klib/string/mutablefuncs.cpp
+++ klib/string/mutablefuncs.cpp
@@ -2,6 +2,22 @@
 //
 // This file contains the functions that modify the string somehow.
 //
+// Copyright:
+//   This file is part of vidad::klib.
+//
+//   vidad::klib is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or (at your option) any later version.
+//
+//   vidad::klib is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Lesser General Public License for more details.
+//
+//   You should have received a copy of the GNU Lesser General Public License
+//   along with vidad::klib.  If not, see <http://www.gnu.org/licenses/>.
+//
 //==-------------------------------------------------------------------------==>
 #include "string.h"
 
@@ -56,9 +72,10 @@ void string::insert( char const* cstrPtr, size_t pos ) {
 		delete[] strPtr_;
 		strPtr_ = tmpPtr;
 	} else {
-		memmove( strPtr_ + pos + len,
-		         strPtr_ + pos  ,
-		         strSize_ - pos );
+		// Move things up:
+		memmove( strPtr_ + pos + len, // By len
+		         strPtr_ + pos  , // From the position inserted at.
+		         strSize_ - pos ); // This number of chars.
 		// Can't use strcpy here, as it would add a \0.
 		memcpy( strPtr_ + pos, cstrPtr, len );
 		strSize_ += len;
@@ -70,7 +87,7 @@ void string::insert( char c, size_t pos ) {
 		// Out of bounds
 		return;
 	if( !allocSize_ ) {
-		// Empty string...
+		// Empty string, allocate some.
 		allocSize_ = roundto_;
 		strPtr_ = new char[allocSize_];
 		strSize_++;
@@ -82,7 +99,7 @@ void string::insert( char c, size_t pos ) {
 		// If necessary, get us more space.
 		allocSize_ += 32;
 		char* tmpPtr = new char[allocSize_];
-		// And copy stuff over, adding the char.
+		// And copy stuff over, adding the char in the middle.
 		memcpy( tmpPtr, strPtr_, pos );
 		tmpPtr[pos] = c;
 		strcpy( tmpPtr + pos + 1, strPtr_ + pos );
