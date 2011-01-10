cac2df13134207a3564d1fa2c13f652ec9656b69
David Buunk <dlbuunk@gmail.com>
2011-01-10 20:22:11 +0100

    added -march=i386 flag

--- global.mk
+++ global.mk
@@ -1,4 +1,4 @@
-COMMON_FLAGS = -Wall -Wextra -O0 -pedantic
+COMMON_FLAGS = -Wall -Wextra -O0 -pedantic -march=i386
 CFLAGS       = $(COMMON_FLAGS) -nostdlib -nostartfiles -nodefaultlibs -nostdinc \
 		-I$(ROOT_DIR)/include
 CXXFLAGS     = $(CFLAGS) -fno-builtin -fno-exceptions -fno-rtti -fno-stack-protector
