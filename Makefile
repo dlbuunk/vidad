1c4848108b0d7fdc3adbcdea8a3faa2d13a7b2f1
D.L. Buunk <dlbuunk@gmail.com>
2011-12-28 01:57:02 +0100

    cpu and memory detection added

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
 	make -C boot_block
 	cp boot_block/boot_block.bin image
-	dd if=/dev/zero of=image seek=1 count=2877
+	dd if=/dev/zero of=image seek=3 count=2877
 	bochs -q
--- boot_block/boot_block.s
+++ boot_block/boot_block.s
@@ -21,7 +21,7 @@
 # Thus, 0.75 kb into the boot block, the loader code starts.
 
 	.text
-	.code16
+	# each function in this file should specify .code16 or .code32 !
 	.global boot_block_entry
 
 	# main table
@@ -114,12 +114,13 @@
 	.byte	0xCF	# page-granular, 32-bit, limit 16:19
 	.byte	0x00	# base 24:31
 
-	# first, the default EXCEP handler
+	# first, the default EXCEP handler (point all IDT entries here)
+	.code32
 	cli
 	movw	$0x0028,%ax
 	movw	%ax,%es
 	movl	$0x000B8000,%ebx
-	movw	$excep_msg,%si
+	movw	$exp_msg,%si
 exp_l:	lodsb
 	cmpb	$0x00,%al
 	je	exp_e
@@ -128,10 +129,193 @@ exp_l:	lodsb
 	jmp	exp_l
 exp_e:
 	hlt
-excep_msg:
+exp_msg:
 	.asciz	"U n h a n d l e d   e x c e p t i o n . "
 
 	# other INT-handlers and helper functions come here
 
+	# real-mode BIOS-printing, %si is pointer to string to print
+print_r:
+	.code16
+	pushw	%ax
+	pushw	%bx
+	movb	$0x0E,%ah
+	movw	$0x0007,%bx
+prr_s:	lodsb
+	cmpb	$0x00,%al
+	je	prr_e
+	int	$0x10
+	jmp	prr_s
+prr_e:	popw	%bx
+	popw	%ax
+	ret
+
+	# Main loader code starts here.
+	# Tasks for the main loader:
+	# => Check CPU type
+	# => Obtain system data from BIOS, and store it.
+	# => A20-gate.
+	# => Setup IDT, and copy GDT.
+	# => Prepare and swich to pmode.
+	# => Re-init some hardware (PIC, PIT, DMAC, FDC)
 boot_block_entry:
+	.data
+msg_cpu:
+	.asciz	"\n\rViOS boot-block loaded.\n\rChecking CPU      "
+msg_cpu_error:
+	.asciz	"FAIL\n\rViOS cannot run on this CPU, aborting."
+	.text
+	.code16
+	movw	$msg_cpu,%si
+	call	print_r
+	pushfw
+
+	pushfw
+	popw	%ax
+	movw	%ax,%cx
+	andw	$0x0FFF,%ax
+	pushw	%ax
+	popfw
+	pushfw
+	popw	%ax
+	and	$0xF000,%ax
+	cmp	$0xF000,%ax
+	jne	check_286
+	popfw
+	movw	$msg_cpu_error,%si
+	call	print_r
+	cli
+	hlt
+
+check_286:
+	orw	$0xF000,%cx
+	pushw	%cx
+	popfw
+	pushfw
+	popw	%ax
+	and	$0xF000,%ax
+	jnz	check_386
+	popfw
+	movw	$msg_cpu_error,%si
+	call	print_r
+	cli
+	hlt
+
+check_386:
+	popfw
+	movb	$0x03,0x2DFE
+	# we have a 386, and are happy with it (for now)
+
+	# check memory && other BIOS data
+	.data
+msg_memory:
+	.asciz	"OK\r\nChecking memory   "
+	.text
+	.code16
+	movw	$msg_memory,%si
+	call	print_r
+
+	# first try int 0x15, %eax = 0x0000E82
+	movw	$0x2C00,%di
+	movl	$0x0000E820,%eax
+	movl	$0x00000000,%ebx
+	movl	$0x00000020,%ecx
+	movl	$0x534D4150,%edx
+	int	$0x15
+	jc	memm_err
+	cmpl	$0x534D4150,%eax
+	jne	memm_err
+memm_loop:
+	addw	$0x20,%di
+	movl	$0x0000E820,%eax
+	movl	$0x00000020,%ecx
+	int	$0x15
+	jc	memm_errc
+	cmpl	$0x00000000,%ebx
+	je	memm_end
+	jmp	memm_loop
+memm_errc:
+	clc
+	subw	$0x0020,%di
+	jmp	memm_end
+memm_err:
+	clc
+	movw	$0x2C00,%di
+memm_end:
+	movw	%di,%ax
+	subw	$0x2C00,%ax
+	movb	$0x20,%cl
+	divb	%cl
+	movb	%al,0x2DFF
+
+	# now try int 0x15, %ax = 0xE801
+	xorw	%cx,%cx
+	xorw	%dx,%dx
+	movw	$0xE801,%ax
+	int	$0x15
+	jc	mem_err
+	cmpb	$0x86,%ah
+	je	mem_err
+	cmpb	$0x80,%ah
+	je	mem_err
+	jcxz	mem_end
+	movw	%cx,%ax
+	movw	%dx,%bx
+mem_err:
+	clc
+	movw	$0x0000,%ax
+	movw	$0x0000,%bx
+mem_end:
+	movw	%bx,0x2DF4
+	movw	%ax,0x2DF6
+
+	# then try int 0x15, %ah = 0x88
+	movb	$0x88,%ah
+	int	$0x15
+	jc	mema_err
+	test	%ax,%ax
+	je	mema_err
+	cmp	$0x86,%ah
+	je	mema_err
+	cmp	$0x80,%ah
+	je	mema_err
+	jmp	mema_end
+mema_err:
+	clc
+	movw	$0x0000,%ax
+mema_end:
+	movw	%ax,0x2DF8
+
+	# finally, try int 0x12
+	xorw	%ax,%ax
+	int	$0x12
+	jc	meml_err
+	jmp	meml_end
+meml_err:
+	clc
+	movw	$0x0000,%ax
+meml_end:
+	movw	%ax,0x2DFA
+
+	# now, add the int 0x11 info
+	xorw	%ax,%ax
+	int	$0x11
+	jc	hw_err
+	jmp	hw_end
+hw_err:
+	clc
+	movw	$0x0000,%ax
+hw_end:
+	movw	%ax,0x2DFC
+
+	# tell everyone that it went OK, and go A20
+	.data
+msg_a20:
+	.asciz	"OK\r\nEnabling A20      "
+	.text
+	.code16
+	movw	msg_a20,%di
+	call	print_r
+
+	cli
 	hlt
