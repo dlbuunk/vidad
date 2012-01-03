a0fa3ed0be2a38902c0b0d0e161155044d7a101e
D.L. Buunk <dlbuunk@gmail.com>
2012-01-03 01:25:23 +0100

    Changed the GPL notices to comply with the requirements of GPLv2.

--- memory/page.cxx
+++ memory/page.cxx
@@ -1,19 +1,21 @@
 //      page.cxx
 //      
 //      Copyright 2012 D.L.Buunk <dlbuunk@gmail.com>
+//
+//      This file is part of ViOS.
 //      
-//      This program is free software; you can redistribute it and/or modify
+//      ViOS is free software; you can redistribute it and/or modify
 //      it under the terms of the GNU General Public License as published by
 //      the Free Software Foundation; either version 2 of the License, or
 //      (at your option) any later version.
 //      
-//      This program is distributed in the hope that it will be useful,
+//      ViOS is distributed in the hope that it will be useful,
 //      but WITHOUT ANY WARRANTY; without even the implied warranty of
 //      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //      GNU General Public License for more details.
 //      
 //      You should have received a copy of the GNU General Public License
-//      along with this program; if not, write to the Free Software
+//      along with ViOS; if not, write to the Free Software
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
 
