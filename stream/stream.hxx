4186e27bcf0d0dd3e0129cfd243f002b67f44a4d
D.L. Buunk <dlbuunk@gmail.com>
2012-06-10 21:26:19 +0200

    Rewritten stream.hxx, added Device, will now add Filesys

--- stream/stream.hxx
+++ stream/stream.hxx
@@ -1,113 +1,162 @@
-//      stream.hxx
+//	stream.hxx
 //
-//      Copyright 2012 D.L.Buunk <dlbuunk@gmail.com>
+//	Copyright 2012 D.L.Buunk <dlbuunk@gmail.com>
 //
 //	This file is part of ViOS.
 //
-//      ViOS is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published by
-//      the Free Software Foundation; either version 2 of the License, or
-//      (at your option) any later version.
+//	ViOS is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
 //
-//      ViOS is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU General Public License for more details.
+//	ViOS is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
 //
-//      You should have received a copy of the GNU General Public License
-//      along with ViOS; if not, write to the Free Software
-//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//      MA 02110-1301, USA.
+//	You should have received a copy of the GNU General Public License
+//	along with ViOS; if not, write to the Free Software
+//	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//	MA 02110-1301, USA.
 
 #ifndef STREAM_HXX
 #define STREAM_HXX
 
-#include "kernel.h"
-#include "util.h"
+#include "kernel.hxx"
+
+#define DEV_ID_LEN 80
 
 namespace stream
 {
 
-enum seek_origin
-{
-	set,
-	cur,
-	end
-};
-
-class Stream_r:
+class Stream
 {
 	public:
+	Stream()
+	{
+		s_id[0] = ';';
+		s_size = 0;
+	}
+	virtual ~Stream() {}
+	char const * get_pid() { return s_pid; }
+	char const * get_id() { return s_id; }
+	qword get_size() { return s_size; }
+	virtual void flush() {};
+	virtual bool eos() = 0;
+
+	protected:
+	// Yes, i know, this can lead to dangerous buffer overflows.
+	char s_id[DEV_ID_LEN];
+	qword s_size;
+};
+
+class Stream_s: public virtual Stream
+{
+	public:
+	Stream_s()
+	{
+		s_pos = 0;
+	}
+	virtual ~Stream_s() {}
+	virtual int seek(long int offset, int origin) = 0;
+	qword get_pos() { return s_pos; }
+	bool eos()
+	{
+		if (s_pos < s_size)
+			return false;
+		else
+			return true;
+	}
+
+	protected:
+	qword s_pos;
+};
+
+class Stream_r: public virtual Stream
+{
+	public:
+	virtual ~Stream_r() {}
 	virtual byte read() = 0;
-	virtual size_t read(byte * ptr, size_t num) = 0;
-	virtual bool eos() = 0;
-	virtual void flush() {};
+	virtual size_t read(byte * p, size_t num) = 0;
 };
 
-class Stream_rs: public Stream_r
+class Stream_rs: public Stream_r, public Stream_s
 {
 	public:
-	virtual int seek_r(long int offset, seek_origin origin) = 0;
+	virtual ~Stream_rs() {}
 };
 
-class Stream_w:
+class Stream_w: public virtual Stream
 {
 	public:
+	virtual ~Stream_w() {}
 	virtual void write(byte val) = 0;
-	virtual size_t write(byte * ptr, size_t num) = 0;
-	virtual bool eos() = 0;
-	virtual void flush() {};
+	virtual size_t write(byte * p, size_t num) = 0;
 };
 
-class Stream_ws: public Stream_w
+class Stream_ws: public Stream_w, public Stream_s
 {
 	public:
-	virtual int seek_w(long int offset, seek_origin origin) = 0;
+	virtual ~Stream_ws() {}
 };
 
-// This class is only intended for low-level devices which cannot seek
-// for reading and writing independently.
-// Note that not all seek_origin values need be supported, though one of
-// them will certainly work.
-class Stream_rwsb: virtual public Stream_r, virtual public Stream_w
+class Stream_rwsb: public Stream_r, public Stream_w, public Stream_s
 {
 	public:
-	virtual int seek_rw(long int offset, seek_origin origin) = 0;
+	virtual ~Stream_rwsb() {}
 };
 
-// And this might be used for a ordinary read/write stream.
-// It is only available for 'full' rws streams, which are supposed
-// to be able to seek independently, heck, they don't even need be
-// connected to the same Stream object!
-struct Stream_rws
-{
-	Stream_rs * r;
-	Stream_ws * w;
-};
-
-// Some ascii convertors, does this seem okay?
-class Stream_ra
+class Device
 {
 	public:
-	Stream_ra(Stream_r *s)
+	Device()
 	{
-		str_ptr = s;
+		type = 0;
+		open_write = false;
+		open_read = 0;
+		reg_subdev = 0;
+		dev_id[0] = ';';
 	}
-	int read(char *);
-	private:
-	Stream_r * str_ptr;
-};
-
-class Stream_wa
-{
-	public:
-	Stream_ra(Stream_w *s)
+	byte get_type() { return type; }
+	bool can_close()
 	{
-		str_ptr = s;
+		if (open_read || open_write)
+			return false;
+		else
+			return true;
 	}
-	int write(char *);
-	private:
-	Stream_w * str_ptr;
+
+	virtual Stream_r open_r() { return 0; }
+	virtual Stream_rs open_rs() { return 0; }
+	virtual Stream_w open_w() { return 0; }
+	virtual Stream_ws open_ws() { return 0; }
+	virtual Stream_rwsb open_rwsb() { return 0; }
+
+	virtual Device open_subdev(int num_subdev)
+	{
+		if (num_subdev = 0 && reg_subdev)
+			return reg_subdev;
+		else
+			return 0;
+	}
+
+	int register_subdev(Device * dev)
+	{
+		if ((type & 0xE0) == 0x80)
+		{
+			reg_subdev = dev;
+			return 0;
+		}
+		else
+			return -1;
+	}
+
+	protected:
+	byte type;
+	bool open_write;
+	unsigned int open_read;
+	Device * reg_subdev;
+	char dev_id[DEV_ID_LEN];
 };
 
 }
