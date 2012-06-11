c8cd01c809b33c354714d5d57965d8d543eb6d31
D.L. Buunk <dlbuunk@gmail.com>
2012-06-11 21:04:19 +0200

    And converted the page_init code, still not tested.

---
+++ boot_fat12/kheader.s
@@ -0,0 +1,32 @@
+#       boot_block.s
+#
+#       Copyright 2011,2012 D.L.Buunk <dlbuunk@gmail.com>
+#
+#       This file is part of ViOS.
+#
+#       ViOS is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       ViOS is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with ViOS; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+	.text
+
+	# this is the kernel header
+	.extern	page_init
+	.long	page_init	# entry point
+	.ascii	"ViOS"		# magic
+	.byte	0x00		# major version
+	.byte	0x01		# minor version
+	.byte	0x00		# minor-minor version
+	.byte	0x01		# number of blocks in page-init code
+	.long	0x00000010	# number of blocks in kernel proper
--- boot_fat12/loader_m.c
+++ boot_fat12/loader_m.c
@@ -146,9 +146,9 @@ void loader_main(void)
 			num_resvd_sectors + num_fat_sectors*num_fats + i);
 
 	// read the kernel
-	for (int i=0; i<16; i++)
+	for (int i=0; i<8; i++)
 	{
-		if (read_file((byte *) 0xE000, "kernel.bin", 0))
+		if (read_file((byte *) (0xE000 + (i<<9)), "kernel.bin", i))
 		{
 			puts("FAIL");
 			return;
@@ -163,7 +163,7 @@ void loader_main(void)
 		void (*)(char *),
 		void (*)(int),
 		void (*)(void)
-	)) (0xE000 + *((word *) 0xE000))))(
+	)) 0xE000))(
 		&read_file,
 		&puts,
 		&timer,
